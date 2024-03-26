8a371d926eb5fd293a107b1083ec0d82c078a389
camlogie <cameronlogie24@gmail.com>
2024-03-25 18:26:15 -0700

    added a request maker class and methods to make simple requests to nhl endpoint

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
 import requests
+
+class RequestMaker:
+
+    def __init__(self):
+        self.base_url = "https://api-web.nhle.com/v1/"
+
+    def make_request(self, endpoint):
+        response = requests.get(self.base_url + endpoint)
+        return response.json()
+
