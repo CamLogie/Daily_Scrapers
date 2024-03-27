e5e6a431e398a18e6347fccd7c1e58f4fd60afac
camlogie <cameronlogie24@gmail.com>
2024-03-27 13:09:58 -0700

    changed request maker class to not use instance methods

--- scraper.py
+++ scraper.py
@@ -2,10 +2,9 @@ import requests
 
 class RequestMaker:
 
-    def __init__(self):
-        self.base_url = "https://api-web.nhle.com/v1/"
+    base_url = "https://api-web.nhle.com/v1/"
 
-    def make_request(self, endpoint):
-        response = requests.get(self.base_url + endpoint)
+    def make_request(endpoint):
+        response = requests.get(RequestMaker.base_url + endpoint)
         return response.json()
 
